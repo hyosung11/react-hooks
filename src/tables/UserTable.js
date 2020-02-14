4190e4b5133e61435dc0aa47b0809765354a3d09
HyoSung Bidol-Lee <hyosung11@me.com>
2020-02-14 17:49:32 -0500

    create UserTable.js

---
+++ src/tables/UserTable.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const UserTable = () => {
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Name Data</td>
+                
+
+            </tr>
+        </tbody>
+    </table>
+}

a491b4ec7ddf164f71f32adbbdaa085042e3f537
HyoSung Bidol-Lee <hyosung11@me.com>
2020-02-14 18:36:27 -0500

    create UserTable

--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-const UserTable = () => {
+const UserTable = () => (
     <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Username</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Name Data</td>
-                
-
-            </tr>
-        </tbody>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name Data</td>
+          <td>Username Data</td>
+          <td>
+            <button className="button muted-button">Edit</button>
+            <button className="button muted-button">Delete</button>
+          </td>
+        </tr>
+      </tbody>
     </table>
-}
+)
+
+export default UserTable;
